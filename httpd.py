f931dabdb1d56ed4516005d1562accbe91d54633
曾 科 <superisaac.ke@gmail.com>
2008-06-06 00:01:00 +0800

    README added flup wsgi server

--- httpd.py
+++ httpd.py
@@ -4,8 +4,17 @@ import settings
 from application import Reactor
 
 def eventlet_server():
+    "Run as standalone wsgi server"
     from eventlet import wsgi, api
     wsgi.server(api.tcp_listener(('', settings.SERVER_PORT)), Reactor)
 
+def flup_server():
+    """Run as fast cgi
+    don't make it a standalone program
+    """
+    from flup.server.fcgi import WSGIServer
+    WSGIServer(Reactor).run()
+
 if __name__ == '__main__':
     eventlet_server()
+    #flup_server()
